36645a840a3ef4f31df915c6952dceafaf495c4b
wingetercaio <62968121+wingetercaio@users.noreply.github.com>
2020-06-20 15:24:21 +0000

    exercício 3

--- m003.c
+++ m003.c
@@ -23,18 +23,22 @@
 
 int is_prime (int a)
 {
-int i;
+int i; /*antecessores de a */
 int n;
-n=0;
+n=0; /*número de divisores*/
 
-for (i==2 ;i<=a; i++)
-if (a%i == 0)
+for (i=2 ;i<=a/2; i++){
+if (a%i == 0){
 n++;
-
-if (n<2)
-return 1;
+break;
+}
+}
+if (n==0)
+{
+return (1);
+}
 else
-return 0;
+return (0);
 
 }
 
