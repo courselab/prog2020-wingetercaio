44fb6ce38e19bfbdbe77a07b3b40dbf16206fdfa
wingetercaio <62968121+wingetercaio@users.noreply.github.com>
2020-07-07 16:03:49 +0000

    Exercício 8

--- m008.c
+++ m008.c
@@ -22,6 +22,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX 256
 
@@ -30,6 +31,61 @@
 
 void lastname (char *s)
 {
+  int i=0;
+  int a=0;
+  int b=0;
+  int c=0;
+
+  char t[MAX];
+
+  strcpy(t,s);
+
+  while (*(t+i)!=0)
+  {
+
+  if (*(t+i)==32 || (*(t+1)==9))
+  {
+    a = i;
+    i++;
+  }
+ 
+  else 
+  i++;
+}
+  
+while (*(t+a+b+1) != 0)
+{
+  if ((*(t+a+1+b) <= 90)&&(*(t+a+b+1) >= 65))
+  {
+    *(s+b)=*(t+a+b+1);
+    b++;
+  }
+ 
+  else
+ 
+    if ((*(t+a+1+b) <= 122) &&(*(t+a+b+1)>=97))
+    {
+  
+      *(s+b) = (*(t+a+b+1)-32);
+      
+      b++;
+    }
+    else
+    
+    b++;
+}
+*(s+b-1)=44;
+
+*(s+b)=32;
+
+while(c<a)
+
+{
+  *(s+b+1) =*(t+c);
+  b++;
+  c++;
+
+}
 }
 
 
